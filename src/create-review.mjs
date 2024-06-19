da34ee6e0d9162de0cbb708f34edac96f3db7c83
Jerome Brown <Jerome@SoliDeoGloria.tech>
2024-06-19 19:35:30 +1200

    Still looking

--- src/create-review.mjs
+++ src/create-review.mjs
@@ -48,7 +48,7 @@ export async function createReview() {
 
   // Find the existing review, if it exists
   core.debug("Listing reviews on the pull request");
-  const { id: reviewId } = octokit.paginate.iterator(
+  const { id: reviewId } = octokit.paginate(
     octokit.rest.pulls.listReviews,
     {
       ...context.repo,
@@ -63,6 +63,8 @@ export async function createReview() {
             review.state === "CHANGES_REQUESTED" &&
             review.body.includes("Terraform Formatting Review")
           ) {
+            core.info("Found existing review");
+            core.debug(`ID: ${review.id}`);
             return review;
           }
         })
@@ -70,8 +72,6 @@ export async function createReview() {
   );
 
   if (reviewId) {
-    core.info("Found existing review");
-    core.debug(`ID: ${reviewId}`);
     core.debug("Dismiss the existing review");
     await octokit.rest.pulls.dismissReview({
       ...context.repo,
