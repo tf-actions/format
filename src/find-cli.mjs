c37b55a9eecfd56afbfba7dbc27f6652c7a6968c
Jerome Brown <Jerome@SoliDeoGloria.tech>
2024-10-09 10:17:37 +1300

    Update findCLI function name

--- src/find-cli.mjs
+++ src/find-cli.mjs
@@ -1,62 +1,62 @@
 import * as core from "@actions/core";
-import * as os from "os";
+import * as os from "node:os";
 import * as io from "@actions/io";
 
 export async function findCLI() {
-  core.debug("Searching for CLI");
+	core.debug("Searching for CLI");
 
-  let cliPath = "";
-  const exeSuffix = os.platform().startsWith("win") ? ".exe" : "";
+	let cliPath = "";
+	const exeSuffix = os.platform().startsWith("win") ? ".exe" : "";
 
-  if (core.getInput("cli-path")) {
-    core.debug(`Looking for CLI path from input: ${cliPath}`);
-    cliPath = core.getInput("cli-path");
-    if (!cliPath.endsWith(exeSuffix)) {
-      cliPath += exeSuffix;
-    }
-    try {
-      await io.which(cliPath, true);
-      return cliPath;
-    } catch {
-      core.warning(`CLI path from input not found: ${cliPath}`);
-      core.debug("Searching for CLI in PATH");
-    }
-  }
+	if (core.getInput("cli-path")) {
+		core.debug(`Looking for CLI path from input: ${cliPath}`);
+		cliPath = core.getInput("cli-path");
+		if (!cliPath.endsWith(exeSuffix)) {
+			cliPath += exeSuffix;
+		}
+		try {
+			await io.which(cliPath, true);
+			return cliPath;
+		} catch {
+			core.warning(`CLI path from input not found: ${cliPath}`);
+			core.debug("Searching for CLI in PATH");
+		}
+	}
 
-  try {
-    // Looking for tofu-bin in case the wrapper from setup-opentofu is in use
-    core.debug("Looking for tofu-bin");
-    await io.which(cliPath, true);
-    core.info(`Using tofu binary at ${cliPath}`);
-    return cliPath;
-  } catch {
-    core.debug("`tofu-bin` not found");
-    try {
-      core.debug("Looking for `tofu`");
-      cliPath = await io.which(`tofu${exeSuffix}`, true);
-      core.info(`Using tofu binary at ${cliPath}`);
-      return cliPath;
-    } catch {
-      core.warning("tofu binary not found");
-    }
-  }
+	try {
+		// Looking for tofu-bin in case the wrapper from setup-opentofu is in use
+		core.debug("Looking for tofu-bin");
+		await io.which(cliPath, true);
+		core.info(`Using tofu binary at ${cliPath}`);
+		return cliPath;
+	} catch {
+		core.debug("`tofu-bin` not found");
+		try {
+			core.debug("Looking for `tofu`");
+			cliPath = await io.which(`tofu${exeSuffix}`, true);
+			core.info(`Using tofu binary at ${cliPath}`);
+			return cliPath;
+		} catch {
+			core.warning("tofu binary not found");
+		}
+	}
 
-  try {
-    // Looking for terraform-bin in case the wrapper from setup-terraform is in use
-    core.debug("Looking for `terraform-bin`");
-    cliPath = await io.which(`terraform-bin${exeSuffix}`, true);
-    core.info(`Using terraform binary at ${cliPath}`);
-    return cliPath;
-  } catch {
-    core.debug("`terraform-bin` not found");
-    try {
-      core.debug("Looking for `terraform`");
-      cliPath = await io.which(`terraform${exeSuffix}`, true);
-      core.info(`Using terraform binary at ${cliPath}`);
-      return cliPath;
-    } catch {
-      core.warning("terraform binary not found");
-    }
-  }
-  throw new Error("CLI not found");
+	try {
+		// Looking for terraform-bin in case the wrapper from setup-terraform is in use
+		core.debug("Looking for `terraform-bin`");
+		cliPath = await io.which(`terraform-bin${exeSuffix}`, true);
+		core.info(`Using terraform binary at ${cliPath}`);
+		return cliPath;
+	} catch {
+		core.debug("`terraform-bin` not found");
+		try {
+			core.debug("Looking for `terraform`");
+			cliPath = await io.which(`terraform${exeSuffix}`, true);
+			core.info(`Using terraform binary at ${cliPath}`);
+			return cliPath;
+		} catch {
+			core.warning("terraform binary not found");
+		}
+	}
+	throw new Error("CLI not found");
 }
--- src/format.mjs
+++ src/format.mjs
@@ -1,18 +1,18 @@
 import * as core from "@actions/core";
 import { context } from "@actions/github";
 import { exec } from "@actions/exec";
-import { findTerraformCLI } from "./find-cli.mjs";
+import { findCLI } from "./find-cli.mjs";
 import { createReview } from "./create-review.mjs";
 
 let createAReview = false;
 if (core.getBooleanInput("create-review", { required: true })) {
-  if (context.payload.pull_request) {
-    createAReview = true;
-  } else {
-    core.warning(
-      "Can only create a review for pull_request events. Ignoring create-review input"
-    );
-  }
+	if (context.payload.pull_request) {
+		createAReview = true;
+	} else {
+		core.warning(
+			"Can only create a review for pull_request events. Ignoring create-review input",
+		);
+	}
 }
 
 core.debug("Starting Terraform formatting validation");
@@ -20,90 +20,91 @@ core.debug("Starting Terraform formatting validation");
 const cli = await findCLI();
 cliName = "";
 switch (cli.split(path.sep).pop()) {
-  case "tofu":
-  case "tofu-bin":
-    cliName = "tofu";
-    break;
-  case "terraform":
-  case "terraform-bin":
-    cliName = "terraform";
-    break;
-  default:
-    cliName = cli.split(path.sep).pop();
+	case "tofu":
+	case "tofu-bin":
+		cliName = "tofu";
+		break;
+	case "terraform":
+	case "terraform-bin":
+		cliName = "terraform";
+		break;
+	default:
+		cliName = cli.split(path.sep).pop();
 }
 
 if (core.getBooleanInput("init", { required: true })) {
-  await exec(cli, ["init", "-backend=false"]);
+	await exec(cli, ["init", "-backend=false"]);
 }
 
 let stdout = "";
 let stderr = "";
 const options = {
-  listeners: {
-    stdout: (data) => {
-      stdout += data.toString();
-    },
-    stderr: (data) => {
-      stderr += data.toString();
-    },
-  },
-  ignoreReturnCode: true,
-  silent: true, // avoid printing command in stdout: https://github.com/actions/toolkit/issues/649
+	listeners: {
+		stdout: (data) => {
+			stdout += data.toString();
+		},
+		stderr: (data) => {
+			stderr += data.toString();
+		},
+	},
+	ignoreReturnCode: true,
+	silent: true, // avoid printing command in stdout: https://github.com/actions/toolkit/issues/649
 };
 let args = ["fmt", "-check"];
 if (core.getBooleanInput("recursive", { required: true })) {
-  args.push("-recursive");
+	args.push("-recursive");
 }
 core.debug(`Running: ${cli} ${args.join(" ")}`);
 const exitCode = await exec(cli, args, options);
 core.debug(`Exit code: ${exitCode}`);
 switch (exitCode) {
-  case 0:
-    core.info("Configuration is formatted correctly");
-    await core.summary
-      .addHeading(":white_check_mark: Formatting is correct")
-      .write();
-    process.exit();
-  case 3:
-    // Terraform fmt returns 3 if there are formatting errors to be corrected
-    break;
-  default:
-    core.setFailed(`${cliName} fmt failed with exit code ${exitCode}`);
+	case 0:
+		core.info("Configuration is formatted correctly");
+		await core.summary
+			.addHeading(":white_check_mark: Formatting is correct")
+			.write();
+		process.exit();
+		break;
+	case 3:
+		// Terraform fmt returns 3 if there are formatting errors to be corrected
+		break;
+	default:
+		core.setFailed(`${cliName} fmt failed with exit code ${exitCode}`);
 }
 const files = stdout.split("\n").filter((line) => line.trim() !== "");
 
-let summary = core.summary
-  .addHeading(":x: Formatting needs to be updated")
-  .addSeparator()
-  .addRaw(`Found ${files.length} files with formatting issues`, true)
-  .addList(files);
+const summary = core.summary
+	.addHeading(":x: Formatting needs to be updated")
+	.addSeparator()
+	.addRaw(`Found ${files.length} files with formatting issues`, true)
+	.addList(files);
 
 if (!createAReview) {
-  summary.addRaw(
-    `Please run \`${cliName} fmt\` locally to fix the formatting issues`,
-    true
-  );
+	summary.addRaw(
+		`Please run \`${cliName} fmt\` locally to fix the formatting issues`,
+		true,
+	);
 }
 summary.write();
 
 // Create annotations for each file with formatting issues
 for (const file of files) {
-  const properties = {
-    title: "Incorrect formatting",
-    file: file,
-  };
-  core.error(`Incorrect formatting in ${file}`, properties);
+	const properties = {
+		title: "Incorrect formatting",
+		file: file,
+	};
+	core.error(`Incorrect formatting in ${file}`, properties);
 }
 
 // Create a review to fix the formatting issues if requested
 if (createAReview) {
-  core.info("Creating a review to fix the formatting issues");
-  args = ["fmt"];
-  if (core.getBooleanInput("recursive", { required: true })) {
-    args.push("-recursive");
-  }
-  await exec(cli, args, { ignoreReturnCode: true, silent: true });
+	core.info("Creating a review to fix the formatting issues");
+	args = ["fmt"];
+	if (core.getBooleanInput("recursive", { required: true })) {
+		args.push("-recursive");
+	}
+	await exec(cli, args, { ignoreReturnCode: true, silent: true });
 
-  await createReview();
+	await createReview();
 }
 core.setFailed("Formatting needs to be updated");
